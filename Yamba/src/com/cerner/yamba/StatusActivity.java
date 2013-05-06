47f6c6488b02df8501950e2c7b7349d77445ddc8
Marko Gargenta <marko@marakana.com>
2013-05-06 10:01:55 -0700

    Implemented simple UI

--- Yamba/src/com/cerner/yamba/StatusActivity.java
+++ Yamba/src/com/cerner/yamba/StatusActivity.java
@@ -1,23 +1,37 @@
 package com.cerner.yamba;
 
-import android.os.Bundle;
 import android.app.Activity;
-import android.view.Menu;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class StatusActivity extends Activity {
+	private static final String TAG = StatusActivity.class.getSimpleName();
+	private Button updateButton;
+	private EditText statusText;
 
-    @Override
+	@Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_status);
+        
+        // Find the views
+        updateButton = (Button) findViewById(R.id.statusUpdateButton);
+        statusText = (EditText) findViewById(R.id.statusText);
+        
+        // Setup button listener
+        updateButton.setOnClickListener( new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				String status = statusText.getText().toString();
+				
+				if(BuildConfig.DEBUG) Log.d(TAG, "onClicked with status: "+status);
+			}
+        	
+        });
     }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.status, menu);
-        return true;
-    }
-    
 }
