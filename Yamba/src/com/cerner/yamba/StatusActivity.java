874ed682dcd9d8b95b49a4fe3258bfba31f2e506
Marko Gargenta <marko@marakana.com>
2013-05-06 14:02:18 -0700

    Minor changes

--- Yamba/src/com/cerner/yamba/StatusActivity.java
+++ Yamba/src/com/cerner/yamba/StatusActivity.java
@@ -1,13 +1,18 @@
 package com.cerner.yamba;
 
 import android.app.Activity;
+import android.app.ProgressDialog;
+import android.graphics.Color;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.marakana.android.yamba.clientlib.YambaClient;
@@ -15,8 +20,11 @@ import com.marakana.android.yamba.clientlib.YambaClientException;
 
 public class StatusActivity extends Activity {
 	private static final String TAG = StatusActivity.class.getSimpleName();
+	private static final int DANGER_ZONE = 50;
 	private Button updateButton;
 	private EditText statusText;
+	private TextView statusCount;
+	private int originalColor;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +34,8 @@ public class StatusActivity extends Activity {
 		// Find the views
 		updateButton = (Button) findViewById(R.id.statusUpdateButton);
 		statusText = (EditText) findViewById(R.id.statusText);
+		statusCount = (TextView) findViewById(R.id.statusCount);
+		originalColor = statusCount.getTextColors().getDefaultColor();
 
 		// Setup button listener
 		updateButton.setOnClickListener(new OnClickListener() {
@@ -33,38 +43,72 @@ public class StatusActivity extends Activity {
 			public void onClick(View v) {
 
 				String status = statusText.getText().toString();
-				if(postToCloudTask!=null && !postToCloudTask.isCancelled()) {
+				if (postToCloudTask != null && !postToCloudTask.isCancelled()) {
 					postToCloudTask.cancel(true);
 				}
 				postToCloudTask = new PostToCloudTask();
 				postToCloudTask.execute(status);
-				
+
 				if (BuildConfig.DEBUG)
 					Log.d(TAG, "onClicked with status: " + status);
+			}
+
+		});
+
+		statusText.addTextChangedListener(new TextWatcher() {
+
+			@Override
+			public void afterTextChanged(Editable s) {
+				int count = 140 - s.length();
+				statusCount.setText(Integer.toString(count));
+		
+				if(count<DANGER_ZONE) {
+					statusCount.setTextColor(Color.RED);
+				} else {
+					statusCount.setTextColor(originalColor);
+				}
+			}
+
+			@Override
+			public void beforeTextChanged(CharSequence s, int start, int count,
+					int after) {
+			}
+
+			@Override
+			public void onTextChanged(CharSequence s, int start, int before,
+					int count) {
 			}
 
 		});
 	}
 
 	PostToCloudTask postToCloudTask;
+
 	@Override
 	protected void onStop() {
 		super.onStop();
-		if(postToCloudTask!=null && !postToCloudTask.isCancelled()) {
+		if (postToCloudTask != null && !postToCloudTask.isCancelled()) {
 			postToCloudTask.cancel(true);
 		}
-		
+
 		Log.d(TAG, "onStop");
 	}
-	
+
 	@Override
 	protected void onDestroy() {
 		super.onDestroy();
 		Log.d(TAG, "onDestroy");
 	}
-	
-	private class PostToCloudTask extends AsyncTask<String, Void, String> {
 
+	private class PostToCloudTask extends AsyncTask<String, Void, String> {
+		private ProgressDialog dialog;
+		
+		// Executes on the UI thread
+		@Override
+		protected void onPreExecute() {
+			dialog = ProgressDialog.show(StatusActivity.this, "Posting", "Please wait...");
+		}
+		
 		// Executes on a non-UI thread
 		@Override
 		protected String doInBackground(String... params) {
@@ -86,6 +130,7 @@ public class StatusActivity extends Activity {
 			Toast.makeText(StatusActivity.this, result, Toast.LENGTH_LONG)
 					.show();
 			statusText.setText("");
+			dialog.dismiss();
 		};
 	};
 
