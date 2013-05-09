15a6d9e3ff7ee128178410d9d50abcdeeba84abd
Marko Gargenta <marko@marakana.com>
2013-05-09 08:33:30 -0700

    Implemented BootReceiver

--- Yamba/src/com/cerner/yamba/NotificationReceiver.java
+++ Yamba/src/com/cerner/yamba/NotificationReceiver.java
@@ -2,10 +2,10 @@ package com.cerner.yamba;
 
 import android.app.Notification;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 
 public class NotificationReceiver extends BroadcastReceiver {
@@ -19,10 +19,18 @@ public class NotificationReceiver extends BroadcastReceiver {
 		NotificationManager notificationManager = (NotificationManager) context
 				.getSystemService(Context.NOTIFICATION_SERVICE);
 
-		Notification notification = new NotificationCompat.Builder(context)
+		PendingIntent operation = PendingIntent.getActivity(context, 0,
+				new Intent(context, MainActivity.class)
+						.addFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT),
+				PendingIntent.FLAG_UPDATE_CURRENT);
+
+		@SuppressWarnings("deprecation")
+		Notification notification = new Notification.Builder(context)
 				.setContentTitle("New Status")
 				.setContentText("There is at least one new status.")
-				.setSmallIcon(android.R.drawable.ic_dialog_alert).build();
+				.setSmallIcon(android.R.drawable.ic_dialog_alert)
+				.setAutoCancel(true).setContentIntent(operation)
+				.getNotification();
 
 		notificationManager.notify(StatusContract.NOTIFICATION_NEW_STATUS,
 				notification);
